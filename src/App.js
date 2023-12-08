8cb6c2614d6991c434adad264335a34d7d4d6598
ShawnRSidwell <shawnrsidwell@gmail.com>
2023-12-08 12:03:21 -0800

    Add toggle and functionality to each list item, lift state to parent

--- src/App.js
+++ src/App.js
@@ -25,25 +25,40 @@ export default function App() {
 }
 
 function Accordion({ data }) {
+  const [curOpen, setCurOpen] = useState(null);
+
   return (
     <div className="accordion">
       {data.map((el, i) => (
-        <AccordionItem title={el.title} text={el.text} num={i} key={el.title} />
+        <AccordionItem
+          curOpen={curOpen}
+          onOpen={setCurOpen}
+          title={el.title}
+          num={i}
+          key={el.title}
+        >
+          {el.text}
+        </AccordionItem>
       ))}
     </div>
   );
 }
 
-function AccordionItem({ num, title, text }) {
-  const [isOpen, setIsOpen] = useState(false);
+function AccordionItem({ num, title, curOpen, onOpen, children }) {
+  const isOpen = num === curOpen;
+
+  function handleToggle() {
+    onOpen(isOpen ? null : num);
+  }
+
   return (
     <div className={`item ${isOpen && "open"}`}>
       <p className="number">{num < 9 ? `0${num + 1}` : num + 1}</p>
       <p className="title">{title}</p>
-      <p className="icon" onClick={() => setIsOpen((isOpen) => !isOpen)}>
+      <p className="icon" onClick={handleToggle}>
         {isOpen ? "-" : "+"}
       </p>
-      {isOpen && <div className="content-box">{text}</div>}
+      {isOpen && <div className="content-box">{children}</div>}
     </div>
   );
 }
